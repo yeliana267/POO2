1019ad35704207ed0e63125f85fc25b6158d3d95
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-02 23:26:53 -0400

    Ultima subida

--- PayrollApp/Program.cs
+++ PayrollApp/Program.cs
@@ -10,6 +10,33 @@ namespace PayrollApp
     {
         static void Main(string[] args)
         {
+            Payroll payroll = new Payroll();
+
+            Console.WriteLine("Seleccione el tipo de empleado (1: Tiempo completo, 2: Medio tiempo): ");
+            string employeeType = Console.ReadLine();
+
+            if (employeeType == "1")
+            {
+                Console.Write("Ingrese el salario base: ");
+                decimal baseSalary = decimal.Parse(Console.ReadLine());
+                decimal netSalary = payroll.CalculateSalaryForFullTime(baseSalary);
+                Console.WriteLine($"Salario neto después de impuestos y bono: {netSalary}");
+            }
+            else if (employeeType == "2")
+            {
+                Console.Write("Ingrese el sueldo por hora: ");
+                decimal hourlyRate = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese las horas trabajadas: ");
+                int hoursWorked = int.Parse(Console.ReadLine());
+
+                decimal netSalary = payroll.CalculateSalaryForPartTime(hourlyRate, hoursWorked);
+                Console.WriteLine($"Salario neto después de impuestos y bono: {netSalary}");
+            }
+            else
+            {
+                Console.WriteLine("Opción no válida.");
+            }
         }
     }
-}
+    }
